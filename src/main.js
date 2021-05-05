4dc7fee3c198870b2801678d2cedadc264843606
niezeyu <1763703458@qq.com>
2021-05-05 17:36:16 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import axios from 'axios'
 //配置axios
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem("token")
+  return config
+})
 Vue.config.productionTip = false
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,13 +1,36 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Form, FormItem } from 'element-ui'
-import { Input } from 'element-ui'
-import { Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Header, Container, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup, Breadcrumb, BreadcrumbItem, Card, Select, Option, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox} from 'element-ui'
+
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
+
+
  
 
