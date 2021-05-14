4ac3c8dade59b9f7c268043851fa6db1e3489d09
niezeyu <1763703458@qq.com>
2021-05-14 11:51:38 +0800

    完成数据报表功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 import Vue from 'vue'
 
 Vue.use(VueRouter)
@@ -57,6 +58,10 @@ const routes = [
     {
       path: "/orders",
       component: Order
+    },
+    {
+      path: "/reports",
+      component: Report
     }
     
   ]
